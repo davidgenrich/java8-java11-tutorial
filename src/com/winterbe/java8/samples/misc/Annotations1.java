d5b6923f0721e6a356755c6857cd4b78814f8516
David Genrich <david.genrich@e2open.com>
2019-12-17 08:51:13 -0800

    Update Annotations1.java
    Updated System.out statements

--- src/com/winterbe/java8/samples/misc/Annotations1.java
+++ src/com/winterbe/java8/samples/misc/Annotations1.java
@@ -35,13 +35,13 @@ public class Annotations1 {
 
     public static void main(String[] args) {
         Hint hint = Person.class.getAnnotation(Hint.class);
-        System.out.println(hint);   // null
-
+        System.out.println("Hint = " + hint);   // null
+        
         Hints hints1 = Person.class.getAnnotation(Hints.class);
-        System.out.println(hints1.value().length);  // 2
+        System.out.println("Hint Value Length = " + hints1.value().length);  // 2
 
         Hint[] hints2 = Person.class.getAnnotationsByType(Hint.class);
-        System.out.println(hints2.length);  // 2
+        System.out.println("Hint Length = " + hints2.length);  // 2
 
     }
 }
